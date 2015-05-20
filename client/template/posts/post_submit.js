2868416aafd25f65eb0d8885d585f2e5e5e0602c
Sintim Nuamah <sintim.nuamah@gmail.com>
2015-05-20 15:15:13 -0700

    Enforce post uniqueness.

--- client/template/posts/post_submit.js
+++ client/template/posts/post_submit.js
@@ -14,6 +14,11 @@ Template.postSubmit.events({
       // display the error to the user and abort
       if (error)
         return alert(error.reason);
+
+      if (result.postExists)
+        alert('This has already been posted');
+
+
       Router.go('postPage', {_id: result._id});  
     });
   }
