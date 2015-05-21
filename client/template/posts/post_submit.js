05c1103f9a2e0d8ba2a5cb562fa99989dbe92cd6
Sintim Nuamah <sintim.nuamah@gmail.com>
2015-05-21 13:56:06 -0700

    Validate post contents on submission.

--- client/template/posts/post_submit.js
+++ client/template/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+
 Template.postSubmit.events({
   'submit form': function(e, template) {
     e.preventDefault();
@@ -8,6 +21,11 @@ Template.postSubmit.events({
       body: $(e.target).find('[name=body]').val(),
       title: $(e.target).find('[name=title]').val(),
     };
+
+
+    var errors = validatePost(post);
+    if (errors.title || errors.body)
+      return Session.set('postSubmitErrors', errors);
 
 
      Meteor.call('postInsert', post, function(error, result) {
